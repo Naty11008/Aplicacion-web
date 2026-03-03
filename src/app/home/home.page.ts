3628e8e565f100633fde2722b36e1db63cf642f4
Naty11008 <utm24090757@utma.edu.mx>
2026-03-03 10:55:19 -0600

    1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel,IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonList],
 })
 export class HomePage {
   constructor() {}
